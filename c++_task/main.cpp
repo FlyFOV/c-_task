f6b7905d1a52db0aed4df1de4b63066a1317bc93
昊天 <tian@haotiandeMacBook-Air.local>
2017-03-23 00:18:19 +0800

    Initial Commit

---
+++ c++_task/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  c++_task
+//
+//  Created by 昊天 on 2017/3/23.
+//  Copyright © 2017年 昊天. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
